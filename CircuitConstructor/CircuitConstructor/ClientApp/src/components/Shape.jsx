d62f03a9c801aafdbb2c49caa93eba7c9e180073
mionalis <mionallis@gmail.com>
2024-01-20 21:48:12 +0600

    feature(Shape): реализован метод, показывающий элементы при клике на фигуру и скрывающий их при клике вне элемента

--- CircuitConstructor/CircuitConstructor/ClientApp/src/components/Shape.jsx
+++ CircuitConstructor/CircuitConstructor/ClientApp/src/components/Shape.jsx
@@ -1,5 +1,5 @@
 import Draggable, { DraggableData } from "react-draggable";
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./styles/canvasStyles.css";
 
 /**
@@ -25,6 +25,7 @@ const Shape = (props) => {
      */
     useEffect(() => {
         setStyle("shape");
+        setIsComponentVisible(false);
         
         if (props.isDragged) {
             setCurrentPosition({x: props.shapeDropPosition.x, y: props.shapeDropPosition.y});
@@ -37,6 +38,7 @@ const Shape = (props) => {
      * @param data
      */
     const onDragHandler = (event: MouseEvent, data: DraggableData) => {
+        setIsComponentVisible(false);
         setStyle("shape-on-drag");
         if (data.x >= 0 && data.y >= 0) {
             setCurrentPosition({x: data.x, y: data.y});
@@ -50,18 +52,48 @@ const Shape = (props) => {
      * @constructor
      */
     const OnDragStopHandler  = (event: MouseEvent, data: DraggableData) => {
+        setIsComponentVisible(true);
         setStyle("shape");
         if (data.x >= 0 && data.y >= 0) {
             setCurrentPosition({x: props.setOnGrid(data.x, 20), y: props.setOnGrid(data.y, 20)});
         }
+    }
+
+    const ref = useRef(null);
+    
+    function useComponentVisible(initialIsVisible) {
+        const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
+
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setIsComponentVisible(false);
+            }
+        };
+
+        useEffect(() => {
+            document.addEventListener('click', handleClickOutside, true);
+            return () => {
+                document.removeEventListener('click', handleClickOutside, true);
+            };
+        }, []);
+
+        return { isComponentVisible, setIsComponentVisible };
+    }
+
+    const { isComponentVisible, setIsComponentVisible } = useComponentVisible(true);
+
+    const onClickHandler = () => {
+        setIsComponentVisible(true);
     }
     
     return (
         <Draggable position={{x: currentPosition.x, y: currentPosition.y}}
                    onDrag={onDragHandler} 
                    onStop ={OnDragStopHandler}>
-            <div className={style}>
+            <div> {isComponentVisible && (<div style={{position:'absolute'}}>Dropdown Component</div>)}
+            <div className={style} ref={ref} onClick={onClickHandler}>
                 {props.post.body}
+            </div>
             </div>
         </Draggable>
     );
